55f512a8d3d71450d85d587d8884e15964a95f9b
Logan Smith <logsmith06@gmail.com>
2021-06-02 22:11:34 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -2,6 +2,8 @@ export function manageFriends(state, action){
   switch (action.type){
     case 'ADD_FRIEND':
       return {friends: state.friends << friend}
+    case 'REMOVE_FRIEND':
+      return {friends: state.friends[state.id].delete}
     default:
       return state;
   }
